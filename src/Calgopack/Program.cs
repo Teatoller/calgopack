1f060c4ec7fc48aa24b5db312d55166f06cad210
Teatoller <steven.otieno1@gmail.com>
2021-02-04 09:45:55 +0300

    (enumerate array): Enumerate array items
    - loop through array items
    
    [Starts #003]

--- src/Calgopack/Program.cs
+++ src/Calgopack/Program.cs
@@ -6,7 +6,50 @@ namespace Calgopack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // async array of strings
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            /* 
+              print each instance of item in array with foreach loop
+            */
+            // foreach (string day in daysOfWeek)
+            // {
+            //     Console.WriteLine(day);
+            // }
+
+            // Print with for loop
+            // for (int i = 0; i < daysOfWeek.Length; i++)
+            // {
+            //     Console.WriteLine(daysOfWeek[i]);
+            // }
+
+            // print with do loop
+            // int index = 0;
+
+            // do
+            // {
+            //     Console.WriteLine(daysOfWeek[index]);
+            //     index++;
+            // } while (index < daysOfWeek.Length);
+
+
+            // Print with while loop
+            int index = 0;
+            while (index < daysOfWeek.Length)
+            {
+                Console.WriteLine(daysOfWeek[index]);
+                index++;
+            }
+
         }
     }
 }
