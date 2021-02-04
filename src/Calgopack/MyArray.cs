ce7dfe1c42728f4344fa9fd73ad07a93c7abb3a9
Teatoller <steven.otieno1@gmail.com>
2021-02-04 16:12:06 +0300

    (array summary):Implement array principles
    - instantiate an array
    - add elements to array
    - out put array elements
    
    [Starts #006]

---
+++ src/Calgopack/MyArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calgopack
+{
+    class MyArray
+    {
+        public void ArrayConcept()
+        {
+            /* n is an array of 10 integers */
+            int[] n = new int[10];
+            int i, j;
+
+            /* initialize elements of array n */
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            /* output each array element's value */
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- src/Calgopack/Program.cs
+++ src/Calgopack/Program.cs
@@ -7,14 +7,22 @@ namespace Calgopack
         static void Main(string[] args)
         {
             /* Enumerate the days of the week */
-            WeekDays days = new WeekDays();
-            days.AmendArray(5, "black Friday");
-            days.Days();
+            // WeekDays days = new WeekDays();
+            // days.AmendArray(5, "black Friday");
+            // days.Days();
 
 
             /* Interact with the terminal to choose a day */
             // SelectAday aDay = new SelectAday();
             // aDay.Aday();
+
+            /* Instantiate an array, populate it and output its elements */
+            // MyArray elements = new MyArray();
+            // elements.ArrayConcept();
+
+            /* Instantiate a two dimentional array, and output its elements */
+            var y = new TwoDimArray();
+            y.TwoDimArrayConcept();
 
         }
     }
